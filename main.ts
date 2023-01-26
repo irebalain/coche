63875f61ab2be5a44e04ed6e92a2c1696fd7f324
irebalain <57016660+irebalain@users.noreply.github.com>
2023-01-26 18:15:20 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "del") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    } else if (receivedString == "tras") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 0)
+    } else if (receivedString == "izq") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    } else if (receivedString == "der") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    }
+})
+radio.setGroup(1)
