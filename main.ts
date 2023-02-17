246864f0bd36e21eec1d502a2695c01b36be060c
irebalain <57016660+irebalain@users.noreply.github.com>
2023-02-17 19:49:01 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,15 +8,19 @@ radio.onReceivedString(function (receivedString) {
         basic.pause(500)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 0)
     } else if (receivedString == "izq") {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         basic.pause(500)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     } else if (receivedString == "der") {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         basic.pause(500)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     }
 })
-radio.setGroup(1)
+radio.setGroup(57)
